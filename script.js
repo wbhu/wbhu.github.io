628823a6616bebdf7e2ebd3902ce050847899fce
wbhu <wbhu@tencent.com>
2026-01-15 14:51:35 +0800

    refine icon

--- script.js
+++ script.js
@@ -1,63 +1,64 @@
-var fontColor = 'black';
+$(document).ready(function() {
+    var fontColor = 'black';
 
-$("#pub").hide();
-$("#pat").hide();
-$("#exp").hide();
-$("#mis").hide();
+    // Hide all sections except home initially
+    $("#pub").hide();
+    $("#pat").hide();
+    $("#exp").hide();
+    $("#mis").hide();
 
-$("#aboutButton").click(function () {
-    $("#pub").hide("slow");
-    $("#exp").hide("slow");
-    $("#pat").hide("slow");
-    $("#mis").hide("slow");
-    $("#about").show("slow");
-});
-$("#pubButton").click(function () {
-    $("#about").hide("slow");
-    $("#exp").hide("slow");
-    $("#pat").hide("slow");
-    $("#mis").hide("slow");
-    $("#pub").show("slow");
-});
-$("#expButton").click(function () {
-    $("#about").hide("slow");
-    $("#pub").hide("slow");
-    $("#pat").hide("slow");
-    $("#mis").hide("slow");
-    $("#exp").show("slow");
-});
-$("#patButton").click(function () {
-    $("#about").hide("slow");
-    $("#pub").hide("slow");
-    $("#exp").hide("slow");
-    $("#mis").hide("slow");
-    $("#pat").show("slow");
-});
-$("#miscButton").click(function () {
-    $("#about").hide("slow");
-    $("#pub").hide("slow");
-    $("#exp").hide("slow");
-    $("#pat").hide("slow");
-    $("#mis").show("slow");
-});
-$("div#menu > ul li").mouseover(function () {
-    $(this).animate({
-        color: fontColor,
-        margin: '0 0 0 20px'
-    }, 200)
-}).mouseout(function () {
-    $(this).animate({
-        color: fontColor,
-        margin: '0'
-    }, 200)
-});
-$("h1").mouseover(function () {
-    $(this).animate({
-        color: '#1C8FE1'
-        // color: '#972'
-    }, 200)
-}).mouseout(function () {
-    $(this).animate({
-        color: fontColor
-    }, 200)
+    // Set initial active state
+    $("#aboutButton li").addClass("active");
+
+    // Helper function to switch sections
+    function switchSection(showId, buttonId) {
+        // Hide all sections
+        $("#about, #pub, #exp, #pat, #mis").hide("slow");
+        // Show target section
+        $(showId).show("slow");
+        // Update active state
+        $("div#menu > ul li").removeClass("active");
+        $(buttonId + " li").addClass("active");
+        // Smooth scroll to content area
+        $('html, body').animate({
+            scrollTop: $("#content-area").offset().top - 20
+        }, 300);
+    }
+
+    // Menu click handlers
+    $("#aboutButton").click(function (e) {
+        e.preventDefault();
+        switchSection("#about", "#aboutButton");
+    });
+    
+    $("#pubButton").click(function (e) {
+        e.preventDefault();
+        switchSection("#pub", "#pubButton");
+    });
+    
+    $("#expButton").click(function (e) {
+        e.preventDefault();
+        switchSection("#exp", "#expButton");
+    });
+    
+    $("#patButton").click(function (e) {
+        e.preventDefault();
+        switchSection("#pat", "#patButton");
+    });
+    
+    $("#miscButton").click(function (e) {
+        e.preventDefault();
+        switchSection("#mis", "#miscButton");
+    });
+
+    // Header name hover effect
+    $("#header-content h1").mouseover(function () {
+        $(this).animate({
+            color: '#1C8FE1'
+        }, 200);
+    }).mouseout(function () {
+        $(this).animate({
+            color: fontColor
+        }, 200);
+    });
 });
